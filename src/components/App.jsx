ab67d025a2eb7f6ae9e357326a499e3cebf3fe54
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-07-08 17:35:32 +0300

    Adds App filtered phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { nanoid } from 'nanoid';
-import { ContactForm } from "./ContactForm/ContactForm";
+// import { ContactForm } from "./ContactForm/ContactForm";
 
 export class App extends Component {
 
