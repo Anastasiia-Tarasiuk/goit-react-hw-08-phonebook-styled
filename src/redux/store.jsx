d5445638aec2fed87c1e192a4d70cb849f0f6e46
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-08-12 19:12:17 +0300

    Bugfix: adds persitst actions ignore

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,27 @@
 import { configureStore, } from '@reduxjs/toolkit';
-import { persistStore} from 'redux-persist';
 import { persistedContactsReducer } from './contactsSlice';
+import {
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
 
 
 export const store = configureStore({
     reducer: {
         contacts: persistedContactsReducer,
     },
+    middleware(getDefaultMiddleware) {
+        return getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          }
+        })
+    }
 });
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
