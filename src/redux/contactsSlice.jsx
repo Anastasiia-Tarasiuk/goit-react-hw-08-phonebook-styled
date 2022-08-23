f9db19a66a8d1721724473dee194ff4aacc94cd7
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-08-23 14:01:13 +0300

    Adds axios

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,12 +1,36 @@
-import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import axios from 'axios';
+
+const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params })
+      return { data: result.data }
+    } catch (axiosError) {
+      let err = axiosError
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      }
+    }
+  }
 
 export const contactsApi = createApi({
     reducerPath: 'contactsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://62fca9546e617f88de9b3751.mockapi.io/' }),
+    baseQuery: axiosBaseQuery({
+        baseUrl: 'https://62fca9546e617f88de9b3751.mockapi.io/',
+    }),
+        
     tagTypes: ['Contact'],
     endpoints: builder => ({
         getContacts: builder.query({
-            query: () => 'contacts',
+            query: () => ({
+                url: 'contacts',
+                method: 'GET',
+            }),
             providesTags: ['Contact'],
         }),
         deleteContact: builder.mutation({
